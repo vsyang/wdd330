7dab2934669adcbea62f0774f59503e5a0be5e52
vsyang <yang.vanessa85@gmail.com>
2025-09-09 18:53:27 -0600

    fixed typos

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,8 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
-
-const element = document.querySelector(".product_list");
+const element = document.querySelector(".product-list");
 
 const productList = new ProductList("Tents", dataSource, element);
 
-productList.inut();
+productList.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -22,7 +22,14 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
-export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product
+}
+
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
   const htmlStrings = list.map(template);
 
   if (clear) {
