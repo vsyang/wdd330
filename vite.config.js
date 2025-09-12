da74c1f6a53fcb61e7bfe2b93277c5d436abbea0
jneberhard <jreberhard3@gmail.com>
2025-09-12 04:28:51 -0600

    individual wk2team changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 // Import the plugin to copy static assets
-import { viteStaticCopy } from "vite-plugin-static-copy";
+//import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   root: "src/",
@@ -13,33 +13,20 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        product1: resolve(
-          __dirname,
-          "src/product_pages/cedar-ridge-rimrock-2.html",
-        ),
-        product2: resolve(__dirname, "src/product_pages/marmot-ajax-3.html"),
-        product3: resolve(
-          __dirname,
-          "src/product_pages/northface-alpine-3.html",
-        ),
-        product4: resolve(
-          __dirname,
-          "src/product_pages/northface-talus-4.html",
-        ),
         product: resolve(__dirname, "src/product_pages/index.html"),
       },
     },
   },
   // Configure the plugin to copy the 'json' and 'images' directories to the build output
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: "json",
-          dest: "",
-        },
-        { src: "images", dest: "" },
-      ],
-    }),
-  ],
+  //plugins: [
+  //  viteStaticCopy({
+  //    targets: [
+  //      {
+  //        src: "json",
+  //        dest: "",
+  //      },
+  //      { src: "images", dest: "" },
+   //   ],
+   // }),
+  //],
 });
